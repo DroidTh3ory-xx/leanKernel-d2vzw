e239f7cf3b5adfdd77dc43fb68ef90b74bd93749
imoseyon <imoseyon@gmail.com>
2012-07-30 23:02:42 -0700

    removed old hybrid stuff

--- cwm.ramdisk/lk/install-recovery.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin/sh
-if test -f /data/lk_perm; then
-  touch /data/lk_once
-  rm /data/lk_counter
-  sync
-  reboot recovery
-fi
--- cwm.ramdisk/lk/lk_boot.sh
+++
@@ -1,4 +0,0 @@
-#!/system/bin/sh
-mount -o remount,rw /system
-busybox cp -a /lk/install-recovery.sh /system/etc
-mount -o remount,ro /system
