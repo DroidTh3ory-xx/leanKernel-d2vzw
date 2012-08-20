10db045c5aabc5fb39df6bb8f4e7ec6ec26f3fef
imoseyon <imoseyon@gmail.com>
2012-08-19 20:56:26 -0700

    build script change for tmo

--- buildlean.sh
+++ buildlean.sh
@@ -11,6 +11,6 @@ find . | cpio -o -H newc | gzip > /tmp/ramdisk.img
 cd ../
 /data/unpack-mkbootimg/mkbootimg --cmdline 'console=null androidboot.hardware=qcom user_debug=31' --base 0x80200000 --ramdiskaddr 0x81500000 --kernel arch/arm/boot/zImage --ramdisk /tmp/ramdisk.img -o zip/boot.img
 cd zip
-zip -r lk_aosp_jb_beta-v${1}.zip *
-mv lk_aosp_jb_beta-v${1}.zip /tmp
+zip -r lk_aosp_jb_tmo_beta-v${1}.zip *
+mv lk_aosp_jb_tmo_beta-v${1}.zip /tmp
 [[ $2 == "upload" ]] && /data/utils/s3_ftpupload2.sh $1
